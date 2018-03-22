4b91f2721547a169cb2f44b7ac3ec0ddeaa1e6a3
LinnierGames <esericksanc@gmail.com>
2018-03-22 10:38:46 -0700

    docs

--- project/routes.py
+++ project/routes.py
@@ -4,12 +4,14 @@ import sys
 # from hashtable import HashTable
 from binarytree import BinarySearchTree
 
-file_of_numbers = "data/phone-numbers-10000.txt"
-file_of_costs = "data/route-costs-100.txt"
+file_of_numbers = "data/phone-numbers-1000.txt"
+file_of_costs = "data/route-costs-106000.txt"
 
 
 def _path_for(relative_path):
-    script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+    """return the absolute path from the script path and appending the relative_path"""
+
+    script_dir = os.path.dirname(__file__)
     abs_file_path = os.path.join(script_dir, relative_path)
 
     return abs_file_path
@@ -38,44 +40,55 @@ class CostNode(object):
 
 
 def _map_route_costs():
+    """create a tree to store the prefix and cost of the routes cost"""
     with open(_path_for(file_of_costs)) as f_costs:
-        arr_routes = f_costs.read().split('\n')
-        arr_routes = filter(lambda item: item != '', arr_routes)
+
+        # list of "+123456,0.02"
+        arr_routes = f_costs.read().splitlines()
 
         tree_costs = BinarySearchTree()
-
-        print 'create tree'
         for a_route in arr_routes:
-            a_route_split = a_route.split(',')
-            a_route_key = a_route_split[0]
-            a_route_cost = a_route_split[1]
 
+            # create a node with prefix, cost key-value pair
+            a_route_key, a_route_cost = a_route.split(',')
             node_cost = CostNode(a_route_key, a_route_cost)
+
             tree_costs.insert(node_cost)
 
-    print 'return tree'
     return tree_costs
 
 
 def cost_of_number(number):
-
+    """Find the longest match of the number given by searching the tree and
+    stopping where a match is found
+    """
     longest_match_key = None
+    cost = 0
 
+    # TODO: search by trimming the number one by one until a match is found
     for a_route in routes_costs_keys:
         if number.startswith(a_route.prefix):
             if longest_match_key is None:
                 longest_match_key = a_route.prefix
+                cost = a_route.cost
             else:
                 if len(a_route.prefix) > len(longest_match_key):
                     longest_match_key = a_route.prefix
+                    cost = a_route.cost
+        else:
+            print 'bad:', repr(number), repr(a_route.prefix)
+    #
 
-    if longest_match_key is not None:
-        return routes_costs.search(CostNode(longest_match_key, None)).cost
-    else:
-        return 0
+    return cost
 
 
 if __name__ == "__main__":
+
+    os.remove(_path_for("data/routes-costs-output.txt"))
+
+    import time
+
+    start = time.time()
 
     # store the route costs into key-value pairs (prefix, cost)
     routes_costs = _map_route_costs()
@@ -85,8 +98,27 @@ if __name__ == "__main__":
 
     # create a list of the numbers to check the cost of
     with open(_path_for(file_of_numbers)) as f_numbers_to_call:
-        phone_numbers_to_call = [line.rstrip('\n') for line in f_numbers_to_call]
+        phone_numbers_to_call = f_numbers_to_call.read().splitlines()
 
+    zero = 0
+    costs = 0
     for a_number in phone_numbers_to_call:
         cost_value = cost_of_number(a_number)
-        print("{},{}".format(a_number, cost_value))
+
+        with open(_path_for("data/routes-costs-output.txt"), 'a') as f:
+            f.write(a_number + ',' + str(cost_value) + '\n')
+
+        if cost_value == 0:
+            zero += 1
+        else:
+            costs += 1
+
+    print routes_costs_keys
+
+    end = time.time()
+
+    print (end - start)
+
+    # 9460 - 1340
+    print zero, "-", costs
+        # print("{},{}".format(a_number, cost_value))
